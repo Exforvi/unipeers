e9d57ae84f91bc247b94509b6993a066cc02dbd2
exforvi <obongexcel5@gmail.com>
2025-08-26 16:09:37 +0100

    styling components,adding fonts and building interactions

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,15 +1,29 @@
 import { howToCreateEvent } from "@/assets/local-data/how-to-create-events";
+import { schools } from "@/assets/local-data/school-list";
+import { formatTimeStampToDate } from "@/utils/format-date.utils";
 import { themeColors } from "@/utils/theme.utils";
 import DateTimePicker from '@react-native-community/datetimepicker';
-import { useState } from "react";
-import { Button, Platform, ScrollView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { formatTimeStampToDate } from "@/utils/format-date.utils";
+import { useEffect, useState } from "react";
+import { Platform, ScrollView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import RNPickerSelect from "react-native-picker-select";
 
 export default function Create () {
   const [title,setTitle] = useState("");
   const [description,setDescription] = useState("");
+  const [venue,setVenue] = useState("");
+  const [schoolOptions,setSchoolOptions] = useState([]);
+  const [selectSchool,setSelectSchool] = useState("");
   const [date, setDate] = useState(new Date());
   const [showDatePicker, setShowDatePicker] = useState(false);
+
+  //make a simple list of schools
+useEffect(() => {
+    const list = []
+    schools.forEach((item) => list.push({
+       label: item.title,
+        value: item.id }))
+    setSchoolOptions(list);
+},[]);
 
   const onChange = (event,selectedDate) => {
     const currentDate = selectedDate || date;
@@ -38,8 +52,7 @@ export default function Create () {
                 <Text className="text-md text-neutral-500">Event description</Text>
                 <TextInput
                 multiline = {true}
-                h
-                style={styles.textarea}
+                style={styles.input}
                 placeholder="title of your event"
                 value={description}
                 onChangeText={(text) => setDescription(text)}/>
@@ -48,11 +61,7 @@ export default function Create () {
               <View>
                 <TouchableOpacity
                   onPress={() => setShowDatePicker(true)}
-                  style={{ backgroundColor: themeColors.darkGray, 
-                  borderRadius: 50, 
-                  paddingHorizontal:16,
-                  paddingVertical: 8,
-                  }}
+                  style={styles.picker}
                   className="flex flex-row items-center justify-between">
                     <Text className="font-bold text-lg text-neutral-100">{formatTimeStampToDate(date)}</Text>
                     <Text className="font-bold text-lg text-neutral-100">Choose event date</Text>
@@ -65,6 +74,26 @@ export default function Create () {
                     onChange={onChange}/>
               )}
               </View>
+
+              <View>
+                <Text className="text-md text-neutral-500">Event venue</Text>
+                <TextInput
+                style={styles.input}
+                placeholder="exact event venue"
+                value={title}
+                onChangeText={(text) => setVenue(text)}/>
+              </View>
+              
+              {schoolOptions.length > 0 && 
+              <View>
+                <Text>Choose school where event will be held</Text>
+                <RNPickerSelect
+                items={schoolOptions}
+                onValueChange={(item) => setSelectSchool(item)}
+                value={selectSchool}
+                />
+              </View>}
+
           </View>
 
           {/* how to create an event - documetation  */}
@@ -92,18 +121,16 @@ const styles = StyleSheet.create({
     },
     input: {
       borderWidth: 2,
-      borderColor: themeColors.darkGray,
-      paddingHorizontal: 16,
-      paddingVertical: 8,
-      borderRadius: 50,
-      fontSize: 16
-    },
-    textarea: {
-      borderWidth: 2,
-      borderColor: themeColors.darkGray,
+      borderColor: themeColors.gray300,
       paddingHorizontal: 16,
       paddingVertical: 8,
       borderRadius: 8,
       fontSize: 16
     },
+    picker: {
+      backgroundColor: themeColors.gray200,
+      paddingHorizontal: 16,
+      paddingVertical: 8,
+    }
 })
+ 
--- components/EventSnippet.jsx
+++ components/EventSnippet.jsx
@@ -1,20 +1,86 @@
-import { View,Text } from "react-native";
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import {themeColors } from "@/utils/theme.utils";
+import { useFonts } from "expo-font"
+import { Dimensions, Image,FlatList, StyleSheet, Text, View, Pressable } from "react-native";
+import {Division, Span} from "@/styled/custom-styled-components";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { Link } from "expo-router";
+import Feather from '@expo/vector-icons/Feather';
+import { useState } from "react";
 
 
-export function EventSnippet ({mainTitle,subTitle,iconName}) {
-    return (
-        <View className="w-ful flex flex-row gap-x-8">
-            <MaterialIcons 
-            name={iconName} 
-            size={36}
-            style={{color: themeColors.darkGreen}} />
-            <View>
-                <Text style={{color: themeColors.darkGreen,fontWeight:"bold"}}>{mainTitle}</Text>
-                <Text>{subTitle}</Text>
+const screenwidth = Dimensions.get("window").width;
+
+export function EventSnippet ({data}) {
+  const [expandText,setExpandText] = useState(false);
+
+  const [fontsLoaded] = useFonts({
+    "Raleway-Bold": require("../assets/fonts/Raleway-Bold.ttf"),
+    "Raleway-Regular": require("../assets/fonts/Raleway-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+        <View className="flex gap-y-2">
+          <Image
+          style={{
+            width: screenwidth,
+            height:400,
+            resizeMode: "cover"
+          }}
+          source={{ uri: data.bannerUrl}}
+          alt="event photo"
+          />
+          {/* interactions  */}
+          <View className="flex flex-row justify-between items-center px-3">
+            {/* left of interactions*/}
+            <View className="flex flex-row gap-x-3">
+              <View className="flex flex-row items-center gap-x-1">
+                <Ionicons name="heart" size={24} color="black"/>
+                <Span className="font-bold text-xs">57</Span>
+              </View>
+
+              <View className="flex flex-row items-center gap-x-1">
+                <Ionicons name="chatbubble" size={24} color="black"/>
+                <Span className="font-bold text-xs">23</Span>
+              </View>
+
+              {/* right of interactions */}
+              <View className="flex flex-row items-center gap-x-1">
+                <MaterialIcons name="loop" size={24} color="black"/>
+                <Span className="font-bold text-xs">2</Span>
+              </View>
             </View>
+
+            <View className="flex flex-row items-center gap-x-3">
+              <Span>N20,000</Span>
+              <Link href={`/event-details/${data.id}`}>
+                 <Feather name="arrow-up-right" size={24} color="black"/>
+              </Link>
+            </View>
+          </View>
+
+          {/* event post section */}
+          <Pressable onPress={() => setExpandText(!expandText)} className="px-3">
+            {expandText
+            ?
+            <Text style={styles.text}>{data.desc}</Text>
+            :
+            <Text style={styles.text}>{data.desc.slice(0,80)}...</Text>}
+          </Pressable>
         </View>
-    )
+  )
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 14,
+    fontFamily: "Raleway-Regular",
+    fontWeight: "bold"
+  }
+})
+
+
 
